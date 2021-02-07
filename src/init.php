25fa852f272c04571d8188fbd418092444f0029a
Rasmus Schultz <rasmus@mindplay.dk>
2021-02-07 11:33:54 +0100

    tweak: use class constant

--- src/init.php
+++ src/init.php
@@ -1,5 +1,7 @@
 <?php
 
-if (false === stream_wrapper_register("composer", "mindplay\\composer_locator\\ComposerStreamWrapper")) {
+use mindplay\composer_locator\ComposerStreamWrapper;
+
+if (false === stream_wrapper_register("composer", ComposerStreamWrapper::class)) {
     error_log("Unable to register Composer stream-wrapper", E_USER_WARNING);
 }
